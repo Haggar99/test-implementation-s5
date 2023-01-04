36a2747ca315f2475e84ece15ff10e53573cf752
Haggar99 <amadydjongol@gmail.com>
2023-01-04 09:08:52 +0100

    feat: server and all package

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express = require('express');
+import bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+
+
+
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(
+    (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 1);
+    next();
+    }
+
+);
+
+export default app;
+
